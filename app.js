bf0b74d4ec3ca8ab82ec1f07a42f038f0db7d4b2
Shreyashmahajan <bevantara947@gmail.com>
2021-10-06 01:57:55 +0530

    js and html done for leap year

---
+++ app.js
@@ -0,0 +1,46 @@
+var dateInput = document.querySelector("#input-date");
+var btnSubmit = document.querySelector("#btn-text");
+var outputDisplay = document.querySelector("#output");
+
+function clickHandler(){
+   
+  var yearFinal = getYear(dateInput.value);
+   var yearConvert = convertToNumber(yearFinal);
+  
+ var showLeapYear =  isLeapYear(yearConvert);
+   if(showLeapYear){
+       showMessage("Hey it's a Leap year");
+   }else {
+       showMessage("Hey it's not a Leap year");
+   }
+  
+}
+
+function getYear(date){
+    var dateConverted = date.replaceAll("-","");
+    dateConverted = dateConverted.slice(0,4);
+    return dateConverted;
+}
+
+function convertToNumber (year){
+   return Number(year);
+}
+
+function isLeapYear(yearCheck){
+    if(yearCheck % 400 === 0){
+       return true;
+    }
+    if(yearCheck % 100 === 0){
+       return false; 
+    }  
+    if(yearCheck % 4 === 0) {
+       return true;
+    }
+    return false;
+}
+function showMessage(msg){
+    outputDisplay.innerText = msg;
+}
+btnSubmit.addEventListener("click", clickHandler );
+
+
